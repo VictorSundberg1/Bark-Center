09547b8f809f24a5e643c49559cc70401e7329a0
VictorSundberg1 <victor.sundberg1@hotmail.com>
2025-09-22 23:21:34 +0200

    remade start-screen / info screen with some mock-content and changed seconds for scale in animation for different info cards

--- src/components/WelcomeCard.jsx
+++ src/components/WelcomeCard.jsx
@@ -4,23 +4,35 @@ function WelcomeCard() {
 	return (
 		<section className="card">
 			<header className="cardHeader slide-in-right">
-				<h2>Welcome to Bark Center</h2>
+				<h2>Welcome to Bark Center🐶</h2>
 			</header>
-			<div className="cardBody scale-in-center">
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Exercitationem alias ab assumenda deserunt minima vel et eum dolore
-					in, reprehenderit, corporis quaerat, doloribus a omnis ipsa! Ducimus
-					unde aliquam aliquid? Hic aspernatur, nesciunt eaque earum culpa,
-					sapiente repellendus numquam voluptatibus ea id tempora amet, nihil
-					voluptas veritatis officia repudiandae deleniti tenetur sed quidem est
-					quia unde. Exercitationem quam distinctio facere aut illo magni, omnis
-					voluptatum necessitatibus corporis eligendi cupiditate voluptates eos,
-					minima nobis qui veniam? Hic placeat quasi ut fugiat praesentium
-					aliquid! Exercitationem nihil doloribus commodi. Nesciunt
-					reprehenderit veritatis totam magnam, expedita consequatur asperiores
-					quam quo fugit, libero labore atque!
-				</p>
+			<div className="cardBody">
+				<section className="info_cards">
+					<div className="info_card scale-in-center">
+						<header>Info 1</header>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+							iure neque illo asperiores soluta minima qui, assumenda eveniet
+							beatae voluptatibus, ullam rerum tempora voluptates et.
+						</p>
+					</div>
+					<div className="info_card scale-in-center2">
+						<header>Info 2</header>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+							iure neque illo asperiores soluta minima qui, assumenda eveniet
+							beatae voluptatibus, ullam rerum tempora voluptates et.
+						</p>
+					</div>
+					<div className="info_card scale-in-center3">
+						<header>Info 3</header>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+							iure neque illo asperiores soluta minima qui, assumenda eveniet
+							beatae voluptatibus, ullam rerum tempora voluptates et.
+						</p>
+					</div>
+				</section>
 			</div>
 		</section>
 	);
