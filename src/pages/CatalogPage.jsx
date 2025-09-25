0135027adef1a7199000caa1aa230aebde70694f
VictorSundberg1 <victor.sundberg1@hotmail.com>
2025-09-25 15:53:38 +0200

    Added search functions with a searchbar for names filtering

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -8,7 +8,9 @@ export default function CatalogPage() {
 	const [dogs, setDogs] = useState([]);
 	const [error, setError] = useState(null);
 	const [showPresent, setShowPresent] = useState(false);
+	const [search, setSearch] = useState('');
 
+	//Get all dogs from JsonBin and add to Array of dogs if successfull
 	useEffect(() => {
 		fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}`)
 			.then((response) =>
@@ -22,26 +24,40 @@ export default function CatalogPage() {
 
 	if (error) return <p>Fel: {String(error.message ?? error)}</p>;
 
+	// Filter dogs array to either show all or only dogs.present = true
 	const filteredPresentDogs = showPresent
 		? dogs.filter((dog) => dog.present)
 		: dogs;
+
+	// Filter dogs array to show dogs.name that contains string in searchbar
+	const filteredDogs = filteredPresentDogs.filter((dog) =>
+		dog.name.toLowerCase().includes(search.toLowerCase())
+	);
 
 	return (
 		<section className="dogCards">
 			<div className="registry_main_card scale-in-center">
 				<h1>All Registered Dogs</h1>
 				<p>Click on a card to see more information from a registered dog🐶</p>
-				<div>
+				<div className="filter_functions">
 					<button
 						onClick={() => setShowPresent((prev) => !prev)}
 						className="show_present_dogs_btn"
 					>
 						{showPresent ? 'Show All Dogs' : 'Show Present Dogs'}
 					</button>
+					<div className="search">
+						<input
+							type="text"
+							placeholder="Search name"
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+						/>
+					</div>
 				</div>
 			</div>
 			<ul>
-				{filteredPresentDogs.map((dog, index) => (
+				{filteredDogs.map((dog, index) => (
 					<RegistryCard key={dog.chipNumber ?? index} dog={dog} />
 				))}
 			</ul>
